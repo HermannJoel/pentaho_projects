f45e9620a94b409453e3fb115f4432b1a9265208
hermann joel <nhermannjoel@yahoo.fr>
2023-06-26 13:23:54 +0200

    env.windows.conf works

--- rh_dwh/framework/bin/init.bat
+++ rh_dwh/framework/bin/init.bat
@@ -1,17 +1,23 @@
 @echo off
-
 REM Set Environment
-SET ROOT_DIR=%~dp0/../..
+CD /d %~dp0\..\..
+SET ROOT_DIR=%CD%
 IF "%PENTAHO_ENV%"=="" (SET PENTAHO_ENV=config-pdi-local)
-
 ECHO Running with "%PENTAHO_ENV%" environment settings
+
+ECHO *** Setting ROOT_DIR to "%ROOT_DIR%" ***
+ECHO *** Setting PENTAHO_ENV to "%PENTAHO_ENV%" ***
+ECHO *** Move to CONF_DIR : "%ROOT_DIR%\rh_dwh-configuration\%PENTAHO_ENV%\env.windows.conf" ***
 
 REM Load Environment Configuration
 FOR /F "usebackq delims=" %%x IN ("%ROOT_DIR%\rh_dwh-configuration\%PENTAHO_ENV%\env.windows.conf") DO (SET %%x)
-
 REM Set Additional Variables
 SET KETTLE_CLIENT_DIR="%PENTAHO_HOME%\"
 SET KETTLE_HOME=%ROOT_DIR%\rh_dwh-configuration\%PENTAHO_ENV%
 SET KETTLE_META_HOME=%ROOT_DIR%\rh_dwh-configuration\%PENTAHO_ENV%
 SET OPT=%OPT% "-DPENTAHO_METASTORE_FOLDER=%KETTLE_META_HOME%"
+
+ECHO *** Setting KETTLE_CLIENT_DIR to $KETTLE_CLIENT_DIR ***
+ECHO *** Setting KETTLE_HOME to $KETTLE_HOME ***
+ECHO *** Setting KETTLE_META_HOME to $KETTLE_META_HOME ***
 
